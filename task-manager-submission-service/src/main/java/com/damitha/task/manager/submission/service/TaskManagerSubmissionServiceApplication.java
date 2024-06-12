d7fc79e904ca805d3089d863ffebc4435287b66c
damitha sahan <sahanchandrasena462@gmail.com>
2024-06-13 02:04:39 +0530

    submission service is added

---
+++ task-manager-submission-service/src/main/java/com/damitha/task/manager/submission/service/TaskManagerSubmissionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.damitha.task.manager.submission.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagerSubmissionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagerSubmissionServiceApplication.class, args);
+	}
+
+}
---
+++ task-manager-submission-service/src/test/java/com/damitha/task/manager/submission/service/TaskManagerSubmissionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.damitha.task.manager.submission.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerSubmissionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
